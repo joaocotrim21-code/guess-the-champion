8caf52842dcfb0f502e049068cbc540e99dd88ee
cotrim <joaocotrim21@gmail.com>
2026-01-15 23:53:23 +0000

    Primeiro commit - base React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>GuessTheChampion ⚽</h1>
+      <p>Bem-vindo ao jogo! 🚀</p>
+      <p>Frontend React já está a correr.</p>
+    </div>
+  );
+}
+
+export default App;
