a8f7e5b02559547df3facf8809c3f7f625297773
cotrim <joaocotrim21@gmail.com>
2026-01-22 17:35:32 +0000

    Adiciona endpoint /api/export-competitions via Pages Functions

---
+++ functions/api/export-competitions.js
@@ -0,0 +1,10 @@
+export async function onRequest(context) {
+  const res = await fetch("https://guess-the-champion.joaocotrim21.workers.dev/export-competitions");
+  const data = await res.text();
+  return new Response(data, {
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+}
